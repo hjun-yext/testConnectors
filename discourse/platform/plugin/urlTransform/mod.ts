18ab81ec8d85085e3ccc290f9e97ad5f9251dea8
Heesun Jun <hjun@yext.com>
2021-12-03 15:36:22 -0500

    update urlTransform

--- discourse/platform/plugin/urlTransform/mod.ts
+++ discourse/platform/plugin/urlTransform/mod.ts
@@ -1,6 +1,6 @@
-declare const domain: string;
+declare var domain: string;
 
-export function fix_avatar_template(avatar_url) {
+export function fix_avatar_template(avatar_url: string) {
     // var avatar_url = avatar_url.format(size='20')
     if (avatar_url.startsWith('https')===false) {
         avatar_url = (`${domain}` + avatar_url)
