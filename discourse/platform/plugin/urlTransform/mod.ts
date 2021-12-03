a1f1d5a7399f92d24109fab6e52c0959475ac2f7
Heesun Jun <hjun@yext.com>
2021-12-03 15:50:16 -0500

    adjust variable usage in transform

--- discourse/platform/plugin/urlTransform/mod.ts
+++ discourse/platform/plugin/urlTransform/mod.ts
@@ -2,8 +2,8 @@ declare var domain: string;
 
 export function fix_avatar_template(avatar_url: string) {
     // var avatar_url = avatar_url.format(size='20')
-    if (avatar_url.startsWith('https')===false) {
-        avatar_url = (`${domain}` + avatar_url)
+    if (avatar_url.startsWith('https')==false) {
+        avatar_url = (domain + avatar_url)
     }
     return avatar_url
 }
