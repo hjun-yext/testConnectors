56d05733db8ade24d65cd03a4492a17fa1615079
Heesun Jun <hjun@yext.com>
2021-12-06 17:33:20 -0500

    separate url replacement and concat

--- discourse/platform/plugin/urlTransform/mod.ts
+++ discourse/platform/plugin/urlTransform/mod.ts
@@ -3,10 +3,12 @@ declare var domainHost: string;
 export function fix_avatar_template(avatar_url: string) {
     // var avatar_url = avatar_url.format(size='20')
     var default_http_prefix = /http:\/\//gi
+    var full_url = avatar_url
     if (avatar_url.startsWith('https')===false) {
         // avatar_url = avatar_url.replace(default_http_prefix, domainHost)
-        avatar_url = (domainHost + avatar_url.replace(default_http_prefix, ''))
-        // avatar_url = (domainHost + avatar_url)
+        full_url = avatar_url.replace(default_http_prefix, '')
+        // avatar_url = (domainHost + )
+        full_url = (domainHost + full_url)
     }
-    return avatar_url
+    return full_url
 }
