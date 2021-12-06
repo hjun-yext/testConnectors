73349f6035d7c7fb19fc3de93dcb503c5eb03921
Heesun Jun <hjun@yext.com>
2021-12-06 15:56:07 -0500

    restore to base url transform

--- discourse/platform/plugin/urlTransform/mod.ts
+++ discourse/platform/plugin/urlTransform/mod.ts
@@ -2,9 +2,10 @@ declare var domainHost: string;
 
 export function fix_avatar_template(avatar_url: string) {
     // var avatar_url = avatar_url.format(size='20')
-    var default_http_prefix = /http:\/\//
-    if (avatar_url.startsWith('http:///')) {
-        avatar_url = (domainHost + avatar_url.replace(default_http_prefix, ''))
+    // var default_http_prefix = /http:\/\//gi
+    if (!avatar_url.startsWith('https')) {
+        // avatar_url = (domainHost + avatar_url.replace(default_http_prefix, ''))
+        avatar_url = (domainHost + avatar_url)
     }
     return avatar_url
 }
