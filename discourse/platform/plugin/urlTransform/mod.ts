49942612c40e3be5529f5c85a8f3b5d3917a4144
Heesun Jun <hjun@yext.com>
2021-12-06 15:41:26 -0500

    fix regex replace

--- discourse/platform/plugin/urlTransform/mod.ts
+++ discourse/platform/plugin/urlTransform/mod.ts
@@ -2,8 +2,9 @@ declare var domainHost: string;
 
 export function fix_avatar_template(avatar_url: string) {
     // var avatar_url = avatar_url.format(size='20')
+    var default_http_prefix = /http:\/\//
     if (avatar_url.startsWith('http:///')) {
-        avatar_url = (domainHost + avatar_url.replace('http://'))
+        avatar_url = (domainHost + avatar_url.replace(default_http_prefix, ''))
     }
     return avatar_url
 }
