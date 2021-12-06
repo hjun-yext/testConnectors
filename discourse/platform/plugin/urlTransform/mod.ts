162e6d3873897ad66b4d998c5d7daebe5046975a
Heesun Jun <hjun@yext.com>
2021-12-06 16:56:57 -0500

    remove http from img urls before adding a host

--- discourse/platform/plugin/urlTransform/mod.ts
+++ discourse/platform/plugin/urlTransform/mod.ts
@@ -4,8 +4,8 @@ export function fix_avatar_template(avatar_url: string) {
     // var avatar_url = avatar_url.format(size='20')
     var default_http_prefix = /http:\/\//gi
     if (!avatar_url.startsWith('https')) {
-        avatar_url = avatar_url.replace(default_http_prefix, domainHost)
-        // avatar_url = (domainHost + avatar_url.replace(default_http_prefix, ''))
+        // avatar_url = avatar_url.replace(default_http_prefix, domainHost)
+        avatar_url = (domainHost + avatar_url.replace(default_http_prefix, ''))
         // avatar_url = (domainHost + avatar_url)
     }
     return avatar_url
