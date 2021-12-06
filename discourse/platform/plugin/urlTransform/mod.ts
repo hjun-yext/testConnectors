cbfa94e28497f1a5f57b61ad64db49626ff57a96
Heesun Jun <hjun@yext.com>
2021-12-06 17:52:35 -0500

    restore to yesterday's function

--- discourse/platform/plugin/urlTransform/mod.ts
+++ discourse/platform/plugin/urlTransform/mod.ts
@@ -2,7 +2,7 @@ declare var domainHost: string;
 
 export function fix_avatar_template(avatar_url: string) {
     // var avatar_url = avatar_url.format(size='20')
-    if (avatar_url.startsWith('https')===false) {
+    if (avatar_url.startsWith('https')==false) {
         avatar_url = (domainHost + avatar_url)
     }
     return avatar_url
