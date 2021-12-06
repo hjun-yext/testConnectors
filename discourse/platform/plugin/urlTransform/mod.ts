39f53144afa507fba2121991563d906789fd78b9
Heesun Jun <hjun@yext.com>
2021-12-06 17:42:27 -0500

    restart from simplest transform

--- discourse/platform/plugin/urlTransform/mod.ts
+++ discourse/platform/plugin/urlTransform/mod.ts
@@ -2,13 +2,8 @@ declare var domainHost: string;
 
 export function fix_avatar_template(avatar_url: string) {
     // var avatar_url = avatar_url.format(size='20')
-    var default_http_prefix = /http:\/\//gi
-    var full_url = avatar_url
     if (avatar_url.startsWith('https')===false) {
-        // avatar_url = avatar_url.replace(default_http_prefix, domainHost)
-        full_url = avatar_url.replace(default_http_prefix, '')
-        // avatar_url = (domainHost + )
-        full_url = (domainHost + full_url)
+        avatar_url = (domainHost + avatar_url)
     }
-    return full_url
+    return avatar_url
 }
