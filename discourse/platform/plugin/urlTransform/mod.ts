992b5ffc95a25cdd9a9e59f17a5d4c5430f391b2
Heesun Jun <hjun@yext.com>
2021-12-06 13:35:08 -0500

    adjust transform for after http:// gets added to image

--- discourse/platform/plugin/urlTransform/mod.ts
+++ discourse/platform/plugin/urlTransform/mod.ts
@@ -2,8 +2,8 @@ declare var domainHost: string;
 
 export function fix_avatar_template(avatar_url: string) {
     // var avatar_url = avatar_url.format(size='20')
-    if (avatar_url.startsWith('https')==false) {
-        avatar_url = (domainHost + avatar_url)
+    if (avatar_url.startsWith('http:///')) {
+        avatar_url = (domainHost + avatar_url.replace('http://'))
     }
     return avatar_url
 }
