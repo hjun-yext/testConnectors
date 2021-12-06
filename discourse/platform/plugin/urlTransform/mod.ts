50918d456e1f54e6936fdd0689ada0d764c6d2b4
Heesun Jun <hjun@yext.com>
2021-12-06 17:16:50 -0500

    restore conditional to use strict false check

--- discourse/platform/plugin/urlTransform/mod.ts
+++ discourse/platform/plugin/urlTransform/mod.ts
@@ -3,7 +3,7 @@ declare var domainHost: string;
 export function fix_avatar_template(avatar_url: string) {
     // var avatar_url = avatar_url.format(size='20')
     var default_http_prefix = /http:\/\//gi
-    if (!avatar_url.startsWith('https')) {
+    if (avatar_url.startsWith('https')===false) {
         // avatar_url = avatar_url.replace(default_http_prefix, domainHost)
         avatar_url = (domainHost + avatar_url.replace(default_http_prefix, ''))
         // avatar_url = (domainHost + avatar_url)
