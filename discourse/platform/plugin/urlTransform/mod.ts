c092e8e428907f610fcdec09998ac1eee4346cf5
Heesun Jun <hjun@yext.com>
2021-12-07 13:16:31 -0500

    use regex to replace any instances of leading / with domainHost

--- discourse/platform/plugin/urlTransform/mod.ts
+++ discourse/platform/plugin/urlTransform/mod.ts
@@ -2,8 +2,11 @@ declare var domainHost: string;
 
 export function fix_avatar_template(avatar_url: string) {
     // var avatar_url = avatar_url.format(size='20')
-    if (avatar_url.startsWith('https')===false) {
-        avatar_url = (domainHost + avatar_url)
-    }
-    return avatar_url
+    let re = /,\//gi
+    let re2 = /^\/|/gi
+    let full_url = avatar_url.replace(re, "," + domainHost + "/").replace(re2, domainHost + "/")
+    // if (avatar_url.startsWith('https')===false) {
+    //     avatar_url = (domainHost + avatar_url)
+    // }
+    return full_url
 }
