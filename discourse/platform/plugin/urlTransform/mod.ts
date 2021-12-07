7a72edd10c186babefe3e1a2d654653f349cc90a
Heesun Jun <hjun@yext.com>
2021-12-07 13:38:38 -0500

    remove or operator from regex

--- discourse/platform/plugin/urlTransform/mod.ts
+++ discourse/platform/plugin/urlTransform/mod.ts
@@ -3,7 +3,7 @@ declare var domainHost: string;
 export function fix_avatar_template(avatar_url: string) {
     // var avatar_url = avatar_url.format(size='20')
     let re = /,\//gi
-    let re2 = /^\/|/gi
+    let re2 = /^\//gi
     let full_url = avatar_url.replace(re, "," + domainHost + "/").replace(re2, domainHost + "/")
     // if (avatar_url.startsWith('https')===false) {
     //     avatar_url = (domainHost + avatar_url)
