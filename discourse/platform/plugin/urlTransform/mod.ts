795c7afc0c9fb5a3e377158a7aef08e49e2fd2e5
Heesun Jun <hjun@yext.com>
2021-12-10 16:57:48 -0500

    update var name to domain

--- discourse/platform/plugin/urlTransform/mod.ts
+++ discourse/platform/plugin/urlTransform/mod.ts
@@ -1,14 +1,10 @@
-declare var domainHost: string;
+declare var domain: string;
 
 export function fix_avatar_template(avatar_url: string) {
-    // var avatar_url = avatar_url.format(size='20')
     let re = /,\//gi
     let re2 = /^\//gi
     let httpRegex = /https?:\/\/|\/.*/gi
-    var sanitizedDomainHost = "https://" + domainHost.replace(httpRegex, '')
+    var sanitizedDomainHost = "https://" + domain.replace(httpRegex, '')
     let full_url = avatar_url.replace(re, "," + sanitizedDomainHost + "/").replace(re2, sanitizedDomainHost + "/")
-    // if (avatar_url.startsWith('https')===false) {
-    //     avatar_url = (domainHost + avatar_url)
-    // }
     return full_url
 }
